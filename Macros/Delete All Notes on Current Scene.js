fa0e1ef576c8de9270d70dc342ffe506965c9f85
Crowguard <99844555+Crowguard@users.noreply.github.com>
2022-09-27 23:39:27 -0500

    Update Delete All Notes on Current Scene.js

--- Macros/Delete All Notes on Current Scene.js
+++ Macros/Delete All Notes on Current Scene.js
@@ -1,4 +1,5 @@
 // Deletes all the notes on the currently viewed scene.
 // Macro by Zhell
+// V9-V10
 const deleteIds = canvas.scene.notes.map(i => i.id);
 await canvas.scene.deleteEmbeddedDocuments("Note", deleteIds);
